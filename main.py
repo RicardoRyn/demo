1ca96e778e7fc043b2711061c7efb4d317fbc443
Ricardo_T8 <ricardoryn1317@gmail.com>
2025-11-18 20:47:49 +0800

    feat: add release automation and new addition function

--- main.py
+++ main.py
@@ -1,7 +1,11 @@
 def main():
     print("Hello from demo!")
-    print("Hello from demo!")
+
+
+def my_add(a: int, b: int) -> int:
+    return a + b
 
 
 if __name__ == "__main__":
     main()
+    my_add(1, 2)
