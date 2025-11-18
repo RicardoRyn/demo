8e1d1d95ca66f13504e48e9f5317b8bed4883fcc
Ricardo_T8 <ricardoryn1317@gmail.com>
2025-11-18 21:23:49 +0800

    feat(math): add subtraction function

--- main.py
+++ main.py
@@ -6,6 +6,11 @@ def my_add(a: int, b: int) -> int:
     return a + b
 
 
+def my_subtract(a: int, b: int) -> int:
+    return a - b
+
+
 if __name__ == "__main__":
     main()
     my_add(1, 2)
+    my_subtract(5, 3)
